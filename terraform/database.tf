f5b3630dc260a00cad3f3991d4e7a6484447a113
Ķábîłáň § <143497959+Kabilan2370@users.noreply.github.com>
2025-12-27 17:42:08 +0530

    Update database.tf

--- terraform/database.tf
+++ terraform/database.tf
@@ -1,3 +1,33 @@
+# aws rds parameter group
+resource "aws_db_parameter_group" "strapi_pg" {
+  name   = "strapi-postgres15"
+  family = "postgres15"
+
+  parameter {
+    name  = "idle_in_transaction_session_timeout"
+    value = "0"
+  }
+
+  parameter {
+    name  = "statement_timeout"
+    value = "0"
+  }
+
+  parameter {
+    name  = "tcp_keepalives_idle"
+    value = "60"
+  }
+
+  parameter {
+    name  = "tcp_keepalives_interval"
+    value = "30"
+  }
+
+  parameter {
+    name  = "tcp_keepalives_count"
+    value = "10"
+  }
+}
 
 # Security Group
 resource "aws_security_group" "strapi_sg2" {
@@ -19,11 +49,24 @@ resource "aws_security_group" "strapi_sg2" {
   }
 }
 
+# aws db subnet group
+resource "aws_db_subnet_group" "strapi_db_subnet" {
+  name       = "docker-strapi-db-subnet"
+  subnet_ids = [
+    "subnet-0c26626bb68ddcbd6",
+    "subnet-0c60862e8ccf3b21a"
+  ]
+
+  tags = {
+    Name = "postgres-strapi-db-subnet"
+  }
+}
+
 resource "aws_db_instance" "strapi" {
-  identifier              = "postgres-database-iden"
+  identifier              = "postgres-sql"
   engine                  = "postgres"
   engine_version          = "15"
-  instance_class          = "db.t3.large"
+  instance_class          = "db.t3.small"
   allocated_storage       = 20
 
   db_name                 = "strapi"
@@ -35,5 +78,6 @@ resource "aws_db_instance" "strapi" {
   skip_final_snapshot     = true
 
   vpc_security_group_ids  = [aws_security_group.strapi_sg2.id] 
+  parameter_group_name = aws_db_parameter_group.strapi_pg.name
 
 }
